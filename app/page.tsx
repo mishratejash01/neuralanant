771f4ae4a1d43c37c801139bf2737b3b72873ec4
ROSHAN KUMAR SINGH <businessmarketing0002@gmail.com>
2026-02-24 03:11:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 import Hero from "@/components/hero";
-import WhyAnant from "@/components/why-anant";
-import HowItWorks from "@/components/how-it-works";
-import Features from "@/components/features";
-import CTABanner from "@/components/cta-banner";
+import SupportersMarquee from "@/components/supporters-marquee";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main>
+    // The main wrapper sets the default off-teal background for the whole page
+    <main className="flex min-h-screen flex-col bg-[#f6fbfb]">
+      <Navbar />
+      
+      {/* 97vh Hero Section */}
       <Hero />
-      <WhyAnant />
-      <HowItWorks />
-      <Features />
-      <CTABanner />
+      
+      {/* Scrolling Marquee sits immediately below the Hero */}
+      <SupportersMarquee />
+      
+      {/* Future sections (like the 'Other LLMs forget' section) will go right here! */}
     </main>
   );
 }
